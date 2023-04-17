09138f7a56754f0cb298e9b731e265979349fd81
Giorgio Zanni <g.zanni@hotmail.com>
2023-04-17 12:01:37 -0400

    feat: update login rewrite

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  async rewrites() {
+    return [
+      {
+        source: '/__/auth/handler',
+        destination: 'https://frontend-core-bfc4e.firebaseapp.com/__/auth/handler',
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig

4179b568b21f819abfbd505c0ec86e0e540948d3
Giorgio Zanni <g.zanni@hotmail.com>
2023-06-19 22:12:11 -0400

    feat: split payment and order logic

--- src/pages/api/orders/status.ts
+++ src/pages/api/orders/status.ts
@@ -1,49 +1,24 @@
 import { ordersClient } from "@frontend-core/server/orders";
-import { paymentsService } from "@frontend-core/server/payments/payments-service";
-import { format } from "date-fns";
-import { es } from "date-fns/locale";
 import { NextApiRequest, NextApiResponse } from "next";
 
-const statusById = new Map([
-  [1, "PENDING"],
-  [2, "PAID"],
-  [3, "REJECTED"],
-  [4, "CANCELED"],
-]);
-
 const getOrderStatus = async (req: NextApiRequest, res: NextApiResponse) => {
-  const { paymentId } = req.query;
+  const { itemId } = req.query;
 
-  if (typeof paymentId !== "string") {
+  if (typeof itemId !== "string") {
     return res.status(404).json({ notFound: true });
   }
 
-  const [payment, order] = await Promise.all([
-    paymentsService.getPaymentStatus(paymentId),
-    ordersClient.get(paymentId),
-  ]).catch((error) => {
-    console.error(error);
-    return [null, null];
-  });
+  const order = await ordersClient.get(itemId);
 
-  if (!order || !payment) {
+  if (!order) {
     return res.status(500).json({ error: true });
   }
-
-  const amount = new Intl.NumberFormat("es-CL", {
-    style: "currency",
-    currency: "CLP",
-  }).format(payment.amount);
 
   return res.status(200).json({
     date: order.start,
     intervalInMinutes: order.intervalInMinutes,
     practitionerName: order.practitionerName,
     practiceAddress: order.practiceAddress,
-    paidAmount: amount,
-    paymentMethod: "Flow",
-    totalAmount: amount,
-    status: statusById.get(payment.status),
   });
 };
 
--- src/pages/api/payments/index.ts
+++ src/pages/api/payments/index.ts
@@ -1,14 +1,28 @@
 import { paymentsService } from "@frontend-core/server/payments/payments-service";
 import { NextApiRequest, NextApiResponse } from "next";
 
+const statusById = new Map([
+  [1, "PENDING"],
+  [2, "PAID"],
+  [3, "REJECTED"],
+  [4, "CANCELED"],
+]);
+
 const getPayment = async (req: NextApiRequest, res: NextApiResponse) => {
-  const { token } = JSON.parse(req.body);
+  const { paymentId } = JSON.parse(req.body);
 
   try {
-    const { commerceOrder } = await paymentsService.getPaymentStatus(token);
+    const payment = await paymentsService.getPaymentStatus(paymentId);
+    const amount = new Intl.NumberFormat("es-CL", {
+      style: "currency",
+      currency: "CLP",
+    }).format(payment.amount);
 
     return res.status(200).json({
-      itemId: commerceOrder,
+      itemId: payment.commerceOrder,
+      amount,
+      status: statusById.get(payment.status),
+      paymentMethod: "Flow",
     });
   } catch (error) {
     console.error(error);

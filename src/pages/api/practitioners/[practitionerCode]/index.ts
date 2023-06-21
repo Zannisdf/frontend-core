2679d1043b2ad0154f1c47819deecf9aa44d6b17
Giorgio Zanni <g.zanni@hotmail.com>
2023-06-21 11:35:51 -0400

    feat: add seo to responses

--- src/pages/api/practitioners/[practitionerCode]/index.ts
+++ src/pages/api/practitioners/[practitionerCode]/index.ts
@@ -1,4 +1,3 @@
-import { timeSlotsService } from "@frontend-core/client/time-slots/time-slots.service";
 import { UserDoc, userService } from "@frontend-core/client/users/user.service";
 import { NextApiRequest, NextApiResponse } from "next";
 
@@ -7,68 +6,81 @@ const DATA_BY_SPECIALTY_ID: Record<string, any> = {
     title: "Pediatría",
     seo: {
       title: "Pediatría | Sobrecupos",
-      description: "",
+      description:
+        "Médicos pediatras altamente capacitados y motivados que ofrecen atención especializada para el cuidado y bienestar de tus hijos.",
     },
   },
   otorrino: {
     title: "Otorrinolaringología",
     seo: {
       title: "Otorrinolaringología | Sobrecupos",
-      description: "",
+      description:
+        "Otorrinos que brindan atención de calidad para el diagnóstico y tratamiento de trastornos relacionados con el oído, la nariz y la garganta",
     },
   },
   oftalmologia: {
     title: "Oftalmología",
     seo: {
       title: "Oftalmología | Sobrecupos",
-      description: "",
+      description:
+        "Especialistas en oftalmología en Sobrecupos. Soluciones personalizadas para tus ojos, miopía, astigmatismo, cataratas y más. Mejora la calidad de tu visión.",
     },
   },
   traumatologia: {
     title: "Traumatología",
     seo: {
       title: "Traumatología | Sobrecupos",
-      description: "",
+      description:
+        "Traumatólogos que ofrecen atención integral para el diagnóstico, tratamiento y rehabilitación de lesiones y trastornos musculoesqueléticos.",
     },
   },
   "medicina-general": {
     title: "Medicina general",
     seo: {
       title: "Medicina general | Sobrecupos",
-      description: "",
+      description:
+        "Encuentra sobrecupos con médicos generales expertos en diagnóstico y tratamiento de enfermedades comunes y crónicas. ¡Mejora tu bienestar y calidad de vida!",
     },
   },
   neurologia: {
     title: "Neurología",
     seo: {
       title: "Neurología | Sobrecupos",
-      description: "",
+      description:
+        "Neurólogos capacitados en el diagnósticos y tratamiento de enfermedades como el Alzheimer, el Parkinson y otros trastornos neurológicos.",
     },
   },
   cirugia: {
     title: "Cirugía",
     seo: {
       title: "Cirugía | Sobrecupos",
-      description: "",
+      description:
+        "Descubre nuestros cirujanos especializados en procedimientos de vanguardia. Atención de calidad para transformar tu bienestar con resultados excepcionales.",
     },
   },
   "inmunologia-y-alergias": {
     title: "Inmunología y alergias",
     seo: {
       title: "Inmunología y alergias | Sobrecupos",
-      description: "",
+      description:
+        "Descubre a los mejores profesionales en enfermedades del sistema inmunológico. Atención de calidad para fortalecer tu salud. ¡Mejora tu bienestar ahora!",
     },
   },
 };
 
 const getViewData = (user: UserDoc) => {
   return {
-    name: `${user.names} ${user.surnames}`,
-    specialty: DATA_BY_SPECIALTY_ID[user.specialty].title,
-    picture: user.picture,
-    insuranceProviders: ["Isapre", "Fonasa", "Particular"],
-    licenseId: user.licenseId,
-    description: user.description,
+    seo: {
+      ...DATA_BY_SPECIALTY_ID[user.specialty].seo,
+      description: user.description,
+    },
+    profile: {
+      name: `${user.names} ${user.surnames}`,
+      specialty: DATA_BY_SPECIALTY_ID[user.specialty].title,
+      picture: user.picture,
+      licenseId: user.licenseId,
+      description: user.description,
+    },
   };
 };
 
